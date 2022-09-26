bbfc7592fb407316c71edfc4c2eb4a5e86f6eeb2
Чекмарев Денис <chekmarevdenchik5@gmail.com>
2022-09-26 22:04:46 +0700

    finish mode

--- Program.java
+++ Program.java
@@ -10,10 +10,20 @@ public class Program {
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("Продукт #1"));
+
         list.add(new Drink("Coca-cola", 110, "шт", 1));
         list.add(new Food("Яблоко", 50, "кг", "10.10.2022"));
         list.add(new HygieneItems("Шампунь", 354, "шт", 1));
         list.add(new KidsProduct("Пюре", 24, "шт", 0));
+
+        list.add(new Bread("Хлеб", 35, "шт", "10.10.2022", "Отруби"));
+        list.add(new Diapers("Памперсы J&B", 500, "шт", 0));
+        list.add(new Egg("Яйцо куринное", 70, "шт", "10.10.2022", 10));
+        list.add(new Limonad("Лимонад", 55, "л", 1));
+        list.add(new Mask("Маска для лица", 150, "шт", 10));
+        list.add(new Milk("Молоко", 70, "л", "10.10.2022", 3.4));
+        list.add(new Pacifier("Соска детская", 60, "шт", 0));
+        list.add(new ToiletPaper("Бумага туалетная", 200, "шт", 4, 2));
         
 
         for (Product product : list) {
